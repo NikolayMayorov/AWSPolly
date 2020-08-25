195c18ac4c87c7352455ede93576a00fd323c569
Nikolay Mayorov <slidings@mail.ru>
2020-08-25 15:58:38 +0300

    Фикс

--- VoiceWordAWS/MainWindow.xaml.cs
+++ VoiceWordAWS/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,16 +15,15 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace VoiceWordAWS
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
         }
     }
 }
---
+++ VoiceWordAWS/Model/PollyModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using Amazon;
+using Amazon.Polly;
+using Amazon.Polly.Model;
+using Amazon.Runtime;
+
+namespace VoiceWordAWS.Model
+{
+    class PollyModel
+    {
+
+        private void TestPolly()
+        {
+            BasicAWSCredentials awsCredentials = new BasicAWSCredentials("AKIAJQIYI4VDPPE43BEA", "akoHSIHS7otYQRNv42CTFkTtOgAtAdxxaYv8es0W");
+
+
+            using (var client = new AmazonPollyClient(awsCredentials, Amazon.RegionEndpoint.USEast1))
+            {
+                var request = new Amazon.Polly.Model.SynthesizeSpeechRequest();
+                request.Text = "Home sweet home";
+                request.OutputFormat = OutputFormat.Mp3;
+                request.LanguageCode = LanguageCode.EnUS;
+                request.VoiceId = VoiceId.Astrid;
+                var response = client.SynthesizeSpeechAsync(request).GetAwaiter().GetResult();
+
+
+                string outpuName = "testName.mp3";
+                FileStream output = File.Open(outpuName, FileMode.OpenOrCreate);
+                response.AudioStream.CopyTo(output);
+                output.Close();
+                //  output.Flush();  //!!!!!!!
+            }
+        }
+    }
+}
